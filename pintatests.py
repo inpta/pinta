11845fe86047dce63a82d0075b54e30944188a1d
Abhimanyu S <abhisrkckl@gmail.com>
2020-05-07 11:28:38 +0530

    moved tests

---
+++ pintatests.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import shutil
+
+def test_dir(folder):
+    print('Checking directory %s for permissions...'%folder, end=" ")
+    if not os.access(folder, os.F_OK):
+        print("%s does not exist... Quitting..."%folder)
+    elif not os.path.isdir(folder):
+        print("%s is not a directory... Quitting..."%folder)
+    elif not os.access(folder, os.R_OK):
+        print("%s not readable... Quitting..."%folder)
+    elif not os.access(folder, os.W_OK):
+        print("%s not writable... Quitting..."%folder)
+    else:
+        print("OK... ")
+        return folder
+    raise OSError()
+    
+def test_read_dir(folder):
+    print('Checking directory %s for permissions...'%folder, end=" ")
+    if not os.access(folder, os.F_OK):
+        print("%s does not exist... Quitting..."%folder)
+    elif not os.path.isdir(folder):
+        print("%s is not a directory... Quitting..."%folder)
+    elif not os.access(folder, os.R_OK):
+        print("%s not readable... Quitting..."%folder)
+    else:
+        print("OK... ")
+        return folder
+    raise OSError()
+     
+def test_input_file(file_path):
+    print('Checking file %s for permissions...'%file_path, end=' ')
+    if not os.access(file_path, os.F_OK):
+        print("%s does not exist... Quitting..."%file_path)
+    elif not os.path.isfile(file_path):
+        print("%s is not a file... Quitting..."%file_path)
+    elif not os.access(file_path, os.R_OK):
+        print("%s not readable... Quitting..."%file_path)
+    else:
+        print("OK... ")
+        return file_path
+    raise OSError()
+
+def check_program(program):
+    print("Checking for %s..."%program, end=" ")
+    program_found = shutil.which(program) is not None
+    if program_found:
+        print("OK...")
+    else:
+        print("Not found... Quitting...")
+    return program_found
